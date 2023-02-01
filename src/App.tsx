5781bc611326157531a3a1bf8785b02fb802255e
Lucas <lucas@stanza.co>
2023-02-01 15:31:23 -0500

    add the .env variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { router } from '@/router'
-import { useCheckUserLoggedIn, useRedirectToLoginWhenNecessary } from './hooks'
+import { useCheckUserLoggedIn } from './hooks'
 
 function App() {
   useCheckUserLoggedIn()
+  console.log(import.meta.env)
 
   return (
     <RouterProvider router={router} />
