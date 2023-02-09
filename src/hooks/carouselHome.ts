7670817151554323a8e919707bacacebcc741dad
Lucas <lucas@stanza.co>
2023-02-09 12:18:45 -0500

    Fix bug with timers

--- src/hooks/carouselHome.ts
+++ src/hooks/carouselHome.ts
@@ -3,18 +3,19 @@ import { useEffect, useState } from 'react'
 export const useHomepageCarousel = (maxIndex: number) => {
   const [selected, setSelected] = useState<number>(0)
   const [automatic, setAutomatic] = useState<boolean>(true)
-
-  let timer: number
+  const [timer, setTimer] = useState<number | undefined>()
 
   useEffect(() => {
     if (automatic) {
-      timer = window.setTimeout(() => {
-        let sInc = selected + 1
-        if (sInc > maxIndex) {
-          sInc = 0
-        }
-        setSelected(sInc)
-      }, 3000)
+      setTimer(
+        window.setTimeout(() => {
+          let sInc = selected + 1
+          if (sInc > maxIndex) {
+            sInc = 0
+          }
+          setSelected(sInc)
+        }, 3000),
+      )
     }
   }, [selected])
 
@@ -22,6 +23,7 @@ export const useHomepageCarousel = (maxIndex: number) => {
     setAutomatic(false)
     clearTimeout(timer)
     setSelected(index)
+    setTimer(undefined)
   }
 
   return { selected, handleSelect }
