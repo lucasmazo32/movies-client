51b2e97c7066991d18c6f9d34475f346b7951d68
Lucas <lucas@stanza.co>
2023-02-09 12:40:15 -0500

    Expand button functionality

--- src/features/LikeShow/LikeButton.tsx
+++ src/features/LikeShow/LikeButton.tsx
@@ -1,22 +1,25 @@
-import { FC } from 'react'
+import { FC, HTMLAttributes } from 'react'
 import { Button } from 'react-daisyui'
 import { ReactComponent as StarIcon } from '@/assets/icons/star.svg'
 import { useAppDispatch } from '@/hooks'
 import { updatePossibleShow } from '@/state'
+import { clx } from '@/utils'
 
-export interface LikeButtonProps {
+export interface LikeButtonProps
+  extends Pick<HTMLAttributes<HTMLButtonElement>, 'className'> {
   imdbId: string
 }
 
-export const LikeButton: FC<LikeButtonProps> = ({ imdbId }) => {
+export const LikeButton: FC<LikeButtonProps> = ({ imdbId, className }) => {
   const dispatch = useAppDispatch()
   const handleClick = () => {
     dispatch(updatePossibleShow(imdbId))
   }
+  const classes = clx('gap-2', className)
   return (
     <Button
       color="primary"
-      className="gap-2"
+      className={classes}
       onClick={handleClick}
     >
       <StarIcon className="w-4" />
